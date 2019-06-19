ebc440059dd5c649618d34d39e7433117a6988ad
harshanarisetty <harshan010@gmail.com>
2019-06-18 23:49:26 -0400

    v 0.0.1

---
+++ src/Landing/Landing.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Landing.css';
+export default class Landing extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello World!</p>
+            </div>
+        )
+    }
+}
